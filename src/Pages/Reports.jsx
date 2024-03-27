3651582a7ed71289caa520eee0ff5816d22645ff
leon-herschel <onikleon24@gmail.com>
2024-03-27 23:48:27 +0800

    Fixed reports page

--- src/Pages/Reports.jsx
+++ src/Pages/Reports.jsx
@@ -1,13 +1,20 @@
-import Nav from '../Components/Navigation/Nav';
-import ProductSalesBarChart from '../Components/Charts/ProductSalesBarChart';
-import YearlyLineChart from '../Components/Charts/YearlyLineChart';
-import { useState } from 'react';
+import Nav from '../Components/Navigation/Nav'
+import ProductSalesBarChart from '../Components/Charts/ProductSalesBarChart'
+import YearlyLineChart from '../Components/Charts/YearlyLineChart'
+import { useState } from 'react'
 
 function Reports({ Toggle }) {
-    const [isChartExpanded, setIsChartExpanded] = useState(false);
+    const [isMonthlyChartExpanded, setIsMonthlyChartExpanded] = useState(false)
+    const [isProductChartExpanded, setIsProductChartExpanded] = useState(false)
 
-    const toggleChartSize = () => {
-        setIsChartExpanded(!isChartExpanded);
+    const toggleChartSize = (chartType) => {
+        if (chartType === 'monthly') {
+            setIsMonthlyChartExpanded(!isMonthlyChartExpanded)
+            setIsProductChartExpanded(false)
+        } else if (chartType === 'product') {
+            setIsProductChartExpanded(!isProductChartExpanded)
+            setIsMonthlyChartExpanded(false)
+        }
     };
 
     return (
@@ -16,27 +23,43 @@ function Reports({ Toggle }) {
             <section className="p-3 fadein">
                 <div className="container-fluid">
                     <div className="row">
-                        <div className="col-md-6">
-                            <h2 className='text-white fs-4'>Monthly Sales Trends</h2>
-                            <div className="text-center p-3 my-2 shadow bg-white rounded" onClick={toggleChartSize}>
-                                <div className={`mx-auto ${isChartExpanded ? 'chart-expanded' : ''}`}>
+                        {isMonthlyChartExpanded && (
+                            <div className="col-12">
+                                <h2 className='text-white fs-4'>Monthly Sales Trends</h2>
+                                <div className="text-center p-3 my-2 shadow bg-white rounded" onClick={() => toggleChartSize('monthly')}>
                                     <YearlyLineChart />
                                 </div>
                             </div>
-                        </div>
-                        <div className="col-md-6">
-                            <h2 className='text-white fs-4'>Product Sales Distribution</h2>
-                            <div className="text-center p-3 my-2 shadow bg-white rounded" onClick={toggleChartSize}>
-                                <div className={`mx-auto ${isChartExpanded ? 'chart-expanded' : ''}`}>
+                        )}
+                        {isProductChartExpanded && (
+                            <div className="col-12">
+                                <h2 className='text-white fs-4'>Product Sales Distribution</h2>
+                                <div className="text-center p-3 my-2 shadow bg-white rounded" onClick={() => toggleChartSize('product')}>
                                     <ProductSalesBarChart />
                                 </div>
                             </div>
-                        </div>
+                        )}
+                        {!isMonthlyChartExpanded && !isProductChartExpanded && (
+                            <>
+                                <div className="col-6">
+                                    <h2 className='text-white fs-4'>Monthly Sales Trends</h2>
+                                    <div className="text-center p-3 my-2 shadow bg-white rounded" onClick={() => toggleChartSize('monthly')}>
+                                        <YearlyLineChart />
+                                    </div>
+                                </div>
+                                <div className="col-6">
+                                    <h2 className='text-white fs-4'>Product Sales Distribution</h2>
+                                    <div className="text-center p-3 my-2 shadow bg-white rounded" onClick={() => toggleChartSize('product')}>
+                                        <ProductSalesBarChart />
+                                    </div>
+                                </div>
+                            </>
+                        )}
                     </div>
                 </div>
             </section>
         </div>
-    );
+    )
 }
 
-export default Reports;
+export default Reports
